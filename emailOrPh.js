c9957f9625f4706f5f99d7073ee2ea3c6a7e0644
Nirvana2512 <63945015+Nirvana2512@users.noreply.github.com>
2021-05-06 23:09:01 +0530

    Add files via upload

---
+++ emailOrPh.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const inputs = Array.from(document.querySelectorAll('input[name=phoneNumber], input[name=email]'));
+    const inputListener = e => inputs.filter(i => i !== e.target).forEach(i => i.required = !e.target.value.length);
+
+    inputs.forEach(i => i.addEventListener('input', inputListener));
+});
